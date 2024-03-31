06543449a9aaa59ce8be707d0e5a31bfc16d1e13
maximramboer <maxim.ramboer@amplifino.com>
2024-03-31 21:05:26 +0200

    initial version

--- index.js
+++ index.js
@@ -1,53 +1,20 @@
-function showabout(){
-    $("#about_container").css("display","inherit");
-    $("#about_container").addClass("animated slideInLeft");
-    setTimeout(function(){
-        $("#about_container").removeClass("animated slideInLeft");
-    },800);
-}
-function closeabout(){
-    $("#about_container").addClass("animated slideOutLeft");
-    setTimeout(function(){
-        $("#about_container").removeClass("animated slideOutLeft");
-        $("#about_container").css("display","none");
-    },800);
-}
-function showwork(){
-    $("#work_container").css("display","inherit");
-    $("#work_container").addClass("animated slideInRight");
-    setTimeout(function(){
-        $("#work_container").removeClass("animated slideInRight");
-    },800);
-}
-function closework(){
-    $("#work_container").addClass("animated slideOutRight");
-    setTimeout(function(){
-        $("#work_container").removeClass("animated slideOutRight");
-        $("#work_container").css("display","none");
-    },800);
-}
+
 function showcontact(){
     $("#contact_container").css("display","inherit");
-    $("#contact_container").addClass("animated slideInUp");
-    setTimeout(function(){
-        $("#contact_container").removeClass("animated slideInUp");
-    },800);
+    var contactContainer = document.getElementById("contact_container");
+    contactContainer.scrollIntoView({ behavior: 'smooth' });
 }
+
 function closecontact(){
-    $("#contact_container").addClass("animated slideOutDown");
-    setTimeout(function(){
-        $("#contact_container").removeClass("animated slideOutDown");
-        $("#contact_container").css("display","none");
-    },800);
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    setTimeout(function() {
+        $("#contact_container").css("display", "none");
+    }, 800); // Adjust the delay time as needed
 }
-setTimeout(function(){
-    $("#loading").addClass("animated fadeOut");
-    setTimeout(function(){
-      $("#loading").removeClass("animated fadeOut");
-      $("#loading").css("display","none");
-      $("#box").css("display","none");
-      $("#about").removeClass("animated fadeIn");
-      $("#contact").removeClass("animated fadeIn");
-      $("#work").removeClass("animated fadeIn");
-    },1000);
-},1500);
+
+const menu = document.getElementById('menu');
+
+// Make the menu visible after a delay
+setTimeout(function() {
+    menu.style.opacity = 1;
+}, 2000); // 2 seconds delay
